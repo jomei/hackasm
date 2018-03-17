9a382dc4f7642902a6bf493de8b42ac7f01f6921
Anatoy Nosov <jomeisama@gmail.com>
2018-03-17 09:57:03 +0300

    Clear inline comment

--- src/reader.rs
+++ src/reader.rs
@@ -4,13 +4,21 @@ use std::io::Read;
 
 use line::Line;
 
+const COMMENT_MARKER: &str = "//";
+
 // из за того что есть Read, можно подложить что угодно, имплементирующее  Read, например Cursor
 pub fn call<R: Read>(content: R) -> Vec<Line> {
     return BufReader::new(content)
         .lines()
         .map(|l| l.expect("Could not parse line").trim().to_string())
-        .filter(|l| !(l.is_empty() || l.starts_with("//")))
+        .filter(|l| !(l.is_empty() || l.starts_with(COMMENT_MARKER)))
+        .map(|l| clear_inline_comment(l))
         .enumerate()
         .map(|(i, line)| Line::new(line, i))
         .collect();
+}
+
+fn clear_inline_comment(line: String) -> String {
+    let split: Vec<&str> =  line.split(COMMENT_MARKER).collect();
+    split[0].trim().to_string()
 }
